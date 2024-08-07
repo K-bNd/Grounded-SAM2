3c6cfbeee614f7ef728518f582af57e311206a64
KB <kevin-brian.n-diaye@epita.fr>
2024-08-07 22:44:20 +0200

    feat(grounding_dino): added predict function

--- grounding_dino.py
+++ grounding_dino.py
@@ -6,14 +6,21 @@ from transformers import AutoProcessor, AutoModelForZeroShotObjectDetection
 from detections import from_transformers, annotate_transformers
 
 
-def get_detections(image, model_id: str = "IDEA-Research/grounding-dino-tiny"):
+def predict(image, text: str, model_id: str = "IDEA-Research/grounding-dino-tiny"):
+    results = get_detections(image, text, model_id=model_id)
+    detections_results = from_transformers(results)
+    return detections_results
+
+
+def get_detections(
+    image, text: str, model_id: str = "IDEA-Research/grounding-dino-tiny"
+):
+
+    # VERY important: text queries need to be lowercased + end with a dot
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     processor = AutoProcessor.from_pretrained(model_id)
     model = AutoModelForZeroShotObjectDetection.from_pretrained(model_id).to(device)
-
-    # VERY important: text queries need to be lowercased + end with a dot
-    text = "a cat. a remote control."
 
     inputs = processor(images=image, text=text, return_tensors="pt").to(device)
     with torch.no_grad():
@@ -32,7 +39,6 @@ def get_detections(image, model_id: str = "IDEA-Research/grounding-dino-tiny"):
 if __name__ == "__main__":
     image_url = "http://images.cocodataset.org/val2017/000000039769.jpg"
     image = Image.open(requests.get(image_url, stream=True).raw)
-    results = get_detections(image)
-    detections_results = from_transformers(results)
+    detections_results = predict(image, "a cat. a remote control.")
     annotated_frame = annotate_transformers(image, detections_results[0])
     annotated_frame.show()
